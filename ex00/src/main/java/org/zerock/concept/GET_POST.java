e2bccc13d6eb08b97a1db4e11edeabc79e1aa01c
owner <owner@LAPTOP-5NP9MNIG>
2018-04-15 23:55:18 +0900

    part2 다시 만들지 않고 part1에 있는 거 쓰려고 하니깐 여기저기 뭐 안 되나봄. org.zerock.web이랑 org.zerock.controller랑 충돌나는 거 같은데 컨트롤+쉬프트_L을 이용해봐도 못 찾겠음. 새로만들어야겠음.

---
+++ ex00/src/main/java/org/zerock/concept/GET_POST.java
@@ -0,0 +1,27 @@
+package org.zerock.concept;
+
+public class GET_POST {
+/*
+게시물의 등록 페이지를 보여준다	
+GET /board/register 
+
+게시물을 실제로 등록한다
+POST /board/register
+
+특정 번호의 게시물을 조회한다.
+GET /board/read?bno=xx
+
+게시물의 수정 화면으로 이동한다.
+GET /board/mod?bno=xx
+
+게시물을 수정한다.
+POST /board/mod
+
+게시물을 삭제한다.
+POST /board/remove
+
+게시물의 목록을 확인한다.
+GET /board/list
+	
+*/	
+}
---
+++ ex00/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,153 @@
+package org.zerock.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.domain.BoardVO;
+//import org.zerock.domain.Criteria;
+//import org.zerock.domain.PageMaker;
+import org.zerock.service.BoardService;
+/*
+스프링 MVC의 Model객체는 해당 매소드에서 뷰(jsp등)에 필요한 데이터를 전달하는 용도로 사용됨
+그래서 메소드 내에서 뷰로 전달할 데이터가 있다면, Model을 파라미터로 선언해주는 것이 편리함
+*/
+
+/*
+등록을 위한 입력 페이지를 보는 경우(GET방식으로 처리)
+실제로 데이터를 처리하는 부분(POST방식으로 처리)
+*/
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+
+  private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+  @Inject
+  private BoardService service;		//BoardService객체를 주입받을 수 있도록 하기 위함
+
+  @RequestMapping(value = "/register", method = RequestMethod.GET)
+  public void registerGET(BoardVO board, Model model) throws Exception {
+
+    logger.info("register get ...........");
+  }
+
+  @RequestMapping(value = "/register", method = RequestMethod.POST)
+  public String registPOST(BoardVO board, Model model) throws Exception {
+  
+   logger.info("regist post ...........");
+   logger.info(board.toString());
+  
+   service.regist(board);
+  
+   model.addAttribute("result", "success");		//model에 값을 넣는데 success라는 String값을 넣는것이다.
+  
+   return "/board/success";
+   			// return "redirect:/board/listAll";
+   }
+
+  /*@RequestMapping(value = "/register", method = RequestMethod.POST)
+  public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
+
+    logger.info("regist post ...........");
+    logger.info(board.toString());
+
+    service.regist(board);
+
+    rttr.addFlashAttribute("msg", "success");
+    return "redirect:/board/listAll";
+  }
+
+  @RequestMapping(value = "/listAll", method = RequestMethod.GET)
+  public void listAll(Model model) throws Exception {
+
+    logger.info("show all list......................");
+    model.addAttribute("list", service.listAll());
+  }
+
+  @RequestMapping(value = "/read", method = RequestMethod.GET)
+  public void read(@RequestParam("bno") int bno, Model model) throws Exception {
+
+    model.addAttribute(service.read(bno));
+  }
+
+  @RequestMapping(value = "/remove", method = RequestMethod.POST)
+  public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) throws Exception {
+
+    service.remove(bno);
+
+    rttr.addFlashAttribute("msg", "SUCCESS");
+
+    return "redirect:/board/listAll";
+  }
+
+  @RequestMapping(value = "/modify", method = RequestMethod.GET)
+  public void modifyGET(int bno, Model model) throws Exception {
+
+    model.addAttribute(service.read(bno));
+  }
+
+  @RequestMapping(value = "/modify", method = RequestMethod.POST)
+  public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
+
+    logger.info("mod post............");
+
+    service.modify(board);
+    rttr.addFlashAttribute("msg", "SUCCESS");
+
+    return "redirect:/board/listAll";
+  }
+
+  @RequestMapping(value = "/listCri", method = RequestMethod.GET)
+  public void listAll(Criteria cri, Model model) throws Exception {
+
+    logger.info("show list Page with Criteria......................");
+
+    model.addAttribute("list", service.listCriteria(cri));
+  }
+
+  @RequestMapping(value = "/listPage", method = RequestMethod.GET)
+  public void listPage(@ModelAttribute("cri") Criteria cri, Model model) throws Exception {
+
+    logger.info(cri.toString());
+
+    model.addAttribute("list", service.listCriteria(cri));
+    PageMaker pageMaker = new PageMaker();
+    pageMaker.setCri(cri);
+    // pageMaker.setTotalCount(131);
+
+    pageMaker.setTotalCount(service.listCountCriteria(cri));
+
+    model.addAttribute("pageMaker", pageMaker);
+  }
+
+  @RequestMapping(value = "/readPage", method = RequestMethod.GET)
+  public void read(@RequestParam("bno") int bno, @ModelAttribute("cri") Criteria cri, Model model) throws Exception {
+
+    model.addAttribute(service.read(bno));
+  }
+
+  @RequestMapping(value = "/removePage", method = RequestMethod.POST)
+  public String remove(@RequestParam("bno") int bno, Criteria cri, RedirectAttributes rttr) throws Exception {
+
+    service.remove(bno);
+
+    rttr.addAttribute("page", cri.getPage());
+    rttr.addAttribute("perPageNum", cri.getPerPageNum());
+    rttr.addFlashAttribute("msg", "SUCCESS");
+
+    return "redirect:/board/listPage";
+  }
+
+  @RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
+  public void modifyPagingGET(@RequestParam("bno") int bno, @ModelAttribute("cri") Criteria cri, Model model)
+      throws Exception {
+
+    model.addAttribute(service.read(bno));
+  }*/
+
+}
